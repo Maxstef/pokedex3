a8bf5261923657ef55196fabe4376a1bb615ffe3
max <casstefaks@gmail.com>
2016-11-01 19:40:29 +0200

    update_day_1_19.40

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,33 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Maurice on 01.11.16.
+//  Copyright © 2016 Maurice. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    
+    func configueCell(_ pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+    
+}
---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,30 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Maurice on 01.11.16.
+//  Copyright © 2016 Maurice. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    fileprivate var _name: String!
+    fileprivate var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    
+}
